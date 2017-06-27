f5347de024d90e5ab86a2384bf58c28e50f8cf76
Nico Axtmann <nico.axtmann95@gmail.com>
2017-06-27 21:24:53 +0200

    Scripts to set up conda

---
+++ install_conda_env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh -O ~/anaconda3.sh
+
+
+export ANACONDA_DIR=/opt/anaconda3
+chmod +x ~/anaconda3.sh
+sudo -u root  ~/anaconda3.sh -b -p $ANACONDA_DIR
+rm -rf ~/anaconda3.sh
+echo "export PATH="$ANACONDA_DIR"/bin:\$PATH" >> ~/.bash_profile
+
+#Create new
+conda create -n datascience python=3.6 --y
+source activate datascience
+
+conda install --y nb_conda
+
+#jupyterthemes
+pip install --upgrade jupyterthemes
+
+# toggle toolbar ON and notebook name ON
+jt -t grade3 -T -N
+
+# Pandas
+conda install --y pandas=0.20.2
+
+# Tensorflow
+conda install --y -c anaconda tensorflow-gpu=1.1.0
+
+# Keras
+conda install --y -c conda-forge keras=2.0.2
